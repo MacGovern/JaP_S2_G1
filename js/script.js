85935e6a0cfa9cefee0f97c5f5d8caad764e1153
lg <leonardoagustingimenez@gmail.com>
2023-08-07 13:21:21 -0300

    fetch

--- js/script.js
+++ js/script.js
@@ -19,3 +19,10 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+fetch(DATA_URL).then(res => {
+  if(res.ok){
+    showData(res.json().then(res => {
+      showData(res.data.students)
+    }))
+  }
+})
